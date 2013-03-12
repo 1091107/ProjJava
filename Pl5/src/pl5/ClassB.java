c4a36a91361ab5b6d0f32114a933699a179b7a24
1091107 Nuno <nuno.t.bernardo@gmail.com>
2013-03-12 17:52:02 +0000

    2ª adição codigo

---
+++ Pl5/src/pl5/ClassB.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl5;
+
+/**
+ *
+ * @author Nuno
+ */
+public class ClassB {
+    
+    private int a;
+}
